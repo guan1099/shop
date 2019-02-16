4482744ea6854c631d04b89eee5a29972afd0abc
guan1099 <18574172416@163.com>
2019-02-16 15:25:15 +0800

    scrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
         '/test/*',
-        '/pay/alipay/*'
+        '/pay/alipay/*',
+        '/weixin/valid1'
     ];
 }
