a857cbcec56db42da6fbfcabb42a735a2aa071fc
guanziqi <18574172416@163.com>
2019-02-19 16:08:19 +0800

    处理用户消息

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -34,6 +34,19 @@ class WeixinController extends Controller
         //var_dump($xml);echo '<hr>';
         $openid = $xml->FromUserName;               //用户openid
         $sub_time = $xml->CreateTime;               //扫码关注时间
+
+
+        // 处理用户发送消息
+        if(isset($xml->MsgType)){
+            if($xml->MsgType=='text'){            //用户发送文本消息
+                $msg = $xml->Content;
+                $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$xml->ToUserName.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. $msg. date('Y-m-d H:i:s') .']]></Content></xml>';
+                echo $xml_response;
+                exit();
+            }
+        }
+
+
         if($event=='subscribe'){
 
             echo 'openid: '.$openid;echo '</br>';
