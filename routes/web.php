ba9b55aa64310572b67b071dd9c3d3909de8c05f
guanziqi <18574172416@163.com>
2019-03-20 16:40:08 +0800

    222

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::post('/userregister','User\UserController@registerdo');
 //登录
 Route::get('/userlogin','User\UserController@login');
 Route::post('/userlogin','User\UserController@logindo');
-Route::any('/userlogini','User\UserController@userLogin');
+Route::any('/test/userlogini','User\UserController@userLogin');
 //购物车
 Route::get('/cart/list','Cart\CartController@list');
 Route::post('/cart/addcart','Cart\CartController@addcart')->middleware('check.cookie');
