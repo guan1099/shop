9696e434649b19b56f7d8e6ce75e608828ac668b
guanziqi <18574172416@163.com>
2019-03-20 16:38:29 +0800

    222

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.mama')
 @section('content')
-    <form action="/userlogin2" method="post">
+    <form action="/userlogini" method="post">
         {{csrf_field()}}
         <font size="24px">登录</font>
         <div class="form-group">
