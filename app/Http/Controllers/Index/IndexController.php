af3a69119c8b9bc52f502b7db151845f1413bff6
guanziqi <18574172416@163.com>
2019-03-22 10:19:30 +0800

    ss

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -16,13 +16,13 @@ class IndexController extends Controller
         ];
         return view('index.index',$data);
     }
-    public function curl(){
+    public function curl(Request $request){
         //print_r($data);
         $url='http://zi.tactshan.com/user/login';
         $ch=curl_init($url);
         curl_setopt($ch, CURLOPT_POST, 1);          //post传输数据
         curl_setopt($ch, CURLOPT_URL, $url);              //设置抓取的url
-        curl_setopt($ch, CURLOPT_POSTFIELDS,['username'=>$_POST['username'],'pwd'=>$_POST['pwd']]);       //用post方法传送参数
+        curl_setopt($ch, CURLOPT_POSTFIELDS,['username'=>$request->post('username'),'pwd'=>$request->post('pwd')]);       //用post方法传送参数
         curl_setopt($ch, CURLOPT_HEADER,0);         //设置头文件的信息作为数据了输出
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//设置获取的信息以文件流的形式返回，而不是直接输出
         $re=curl_exec($ch);
